514fd7f67758cf09829ad5de0ac33bc212922cb4
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2024-09-22 19:46:02 +0530

    initial commit

--- practice2.py
+++ practice2.py
@@ -1,8 +1,7 @@
-num = int(input("Enter a number: "))
-
-if num > 0:
-    print(f"{num} is positive number.")
+num=int(input("enter the number:"))
+if num>0:
+    print(f"{num} is positive number")
 elif num<0:
-    print(f"{num} is negative number.")
-else: 
-    print(f"{num} is zero.");   
+    print(f"{num} is negative number")
+else:
+    print(f"{num} is zero")
