d765824ba85b46cd0369a779df2e633a6cf7fe7e
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-02-09 22:51:19 +0530

    initial commit

--- practice10.py
+++ practice10.py
@@ -7,3 +7,4 @@ elif num2<=num1 and num2<=num3:
      print(f"the minimum of {num1} and {num2} and {num3} is {num2}")
 else:
       print(f"the minimum of {num1} and {num2} and {num3} is {num3}")
+      
---
+++ practice11.py
@@ -0,0 +1,14 @@
+num = int(input("Enter the number: "))
+
+temp = num  
+rev = 0 
+
+while temp > 0:
+    digit = temp % 10  
+    rev = rev * 10 + digit 
+    temp //= 10 
+
+if num == rev:
+    print(f"{num} is a palindrome")
+else:
+    print(f"{num} is not a palindrome")
